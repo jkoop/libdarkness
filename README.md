f898fcc77962625ba1c9bfee8eace9495baf9406
Joe Koop <joe@joekoop.com>
2021-02-06 13:36:43 -0600

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # libdarkness
 Auto dark mode library
+
+For a demonstration, visit [my website](https://joekoop.com/).
+
+## Installation (CDN)
+### Dependencies:
++ jQuery
+
+```html
+<!-- Stylesheet -->
+<link rel="preload, preconnect, dns-prefetch, stylesheet" href="https://static.joekoop.com/libdarkness/main.css" />
+
+<!-- Script; should be first child of <body/> -->
+<script rel="preload, preconnect, dns-prefetch" src="https://static.joekoop.com/libdarkness/main.js" type="module"></script>
+```
+
+## Installation (self-hosted)
+### Dependencies:
++ jQuery
+
+```html
+<!-- Stylesheet -->
+<link rel="preload, preconnect, dns-prefetch, stylesheet" href="path/to/main.css" />
+
+<!-- Script; should be first child of <body/> -->
+<script rel="preload, preconnect, dns-prefetch" src="path/to/main.js" type="module"></script>
+```
+
+## Usage
+
+```html
+<!-- Status and control; place where ever you like; prob'ly in the page footer -->
+<span id="libdarkness-status">Auto dark mode</span> - <span id="libdarkness-control">Change</span>
+
+```
+
+For the dark mode, libdarkness adds the libdarkness class to `<body>`. You are free to customise and add styling to fit your needs.
---
+++ main.js
@@ -0,0 +1,71 @@
+/*  libdarkness copyright Joe Koop 2021
+    released under GPL3 license
+    https://github.com/jkoop/libdarkness */
+
+function dark(){
+	$('body').addClass('libdarkness');
+	//$('meta[name="theme-color"]').attr('content', '#000');
+  //lightIcon.remove();
+  //$('head').append(darkIcon);
+}
+
+function light(){
+	$('body').removeClass('libdarkness');
+	//$('meta[name="theme-color"]').attr('content', '#FFF');
+  //darkIcon.remove();
+  //$('head').append(lightIcon);
+}
+
+function init(){
+  //if($('meta[name="theme-color"]').length == 0){
+  //  $('head').append('<meta name="theme-color">');
+  //}
+
+  if(localStorage.getItem('libdarkness') != 'dark' && localStorage.getItem('libdarkness') != 'light' && localStorage.getItem('libdarkness') !== null){
+ 	  localStorage.removeItem('libdarkness');
+  }else if(localStorage.getItem('libdarkness') == 'dark'){
+	  dark();
+	  $('#libdarkness-status').text('Forced dark mode');
+  }else if(localStorage.getItem('libdarkness') == 'light'){
+	  light();
+	  $('#libdarkness-status').text('Forced light mode');
+  }else{
+    auto();
+  }
+}
+
+function auto() {
+  if(localStorage.getItem('libdarkness') === null){
+    if(listener.matches){
+	  	dark();
+    }else{
+  		light();
+    }
+  }
+}
+
+$(document).ready(function(){
+	init();
+
+	$('#libdarkness-control').click(function(){
+		if(localStorage.getItem('libdarkness') == 'dark'){
+			$('#libdarkness-status').text('Forced light mode');
+			localStorage.setItem('libdarkness', 'light');
+			light();
+		}else if(localStorage.getItem('libdarkness') == 'light'){
+			$('#libdarkness-status').text('Auto dark mode');
+		  localStorage.removeItem('libdarkness');
+			auto();
+		}else if(localStorage.getItem('libdarkness') === null){
+			$('#libdarkness-status').text('Forced dark mode');
+			localStorage.setItem('libdarkness', 'dark');
+			dark();
+		}
+	});
+});
+
+//var lightIcon = $('link#libdarkness-lighticon');
+//var darkIcon  = $('link#libdarkness-darkicon');
+var listener  = window.matchMedia('(prefers-color-scheme: dark)');
+listener.addListener(auto);
+init();
